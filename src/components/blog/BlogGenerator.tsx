852e997767b8ad9ca787d776b4f4f93feb786636
MOHAMED MARZOUK <mohamedtm01123@gmail.com>
2025-08-04 03:58:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/blog/BlogGenerator.tsx
+++ src/components/blog/BlogGenerator.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useActionState, useFormStatus } from 'react';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { createBlogPostAction, type FormState } from '@/app/blog/actions';
 import { useEffect } from 'react';
 import { useToast } from '@/hooks/use-toast';
