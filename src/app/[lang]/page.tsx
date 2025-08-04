62ebc7f1c5f18414333ba1f326bfcbdd2a856d3d
MOHAMED MARZOUK <mohamedtm01123@gmail.com>
2025-08-04 04:19:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { projects, services } from '@/lib/data';
 import { ArrowRight, Award, Building, Users } from 'lucide-react';
 import Image from 'next/image';
